98887771ca710a5c8ea638322f9bf18388e9f35c
perezjae <perezjae@gmail.com>
2023-04-29 03:35:29 +0000

    Se realiza ajustes de vista, controlador y modelo

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -9,7 +9,18 @@ sap.ui.define([
 
         return Controller.extend("logaligroup.invoices.controller.MainView", {
             onInit: function () {
+                const oJSONModel = new sap.ui.model.json.JSONModel();
+                const oView = this.getView();
+                oJSONModel.loadData("../model/SelectionScreenMenu.json");
+                oView.setModel(oJSONModel, "selectionScreen");
+            },
+            onFilter: function (oEvent) {
 
+            },
+            onClearFilter: function () {
+                const oModelSelScreen = this.getView().getModel("selectionScreen");
+                oModelSelScreen.setProperty("/ShipName", "");
+                oModelSelScreen.setProperty("/CountryKey", "")
             }
         });
     });
